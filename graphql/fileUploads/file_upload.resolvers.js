547b7ddc1915f3d6ff49ce9cd6ccab533edefd8d
Abdul Jabbar Hafizh <jabbarworld1@gmail.com>
2022-04-17 06:11:47 +0700

    Revert "add package fs-capacitor to fix create file stream to server"
    This reverts commit ab164bd2d4f955e6e90fc9e43093672045253dc7.

--- graphql/fileUploads/file_upload.resolvers.js
+++ graphql/fileUploads/file_upload.resolvers.js
@@ -5,7 +5,7 @@ const FileUploadUtilities = require('./file_upload.utilities');
 async function SingleUpload(parent, args) {
   return args.file.then(async (file) => {
     let { filename, createReadStream } = await file;
-    let originalName = filename;
+    let originalnName = filename;
     let extention = path.extname(filename);
     let newFilename = await FileUploadUtilities.SaveFileToServer(
       path.parse(filename.split(' ').join('-').split('/').join('-').split('#').join('-').split('%').join('-')).name,
@@ -15,7 +15,7 @@ async function SingleUpload(parent, args) {
     );
 
     return {
-      original_file_name: originalName,
+      original_file_name: originalnName,
       bucket_file_name: newFilename.originalname + extention,
       file_url: `${process.env.SERVER_ENV === 'production' ? process.env.URL : process.env.API_BASE}/fileuploads/${
         newFilename.originalname
