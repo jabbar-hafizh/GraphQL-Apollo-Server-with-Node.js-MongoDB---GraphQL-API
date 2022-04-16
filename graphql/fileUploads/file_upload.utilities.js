a86db4fe85737d31d4e0f9d0c08e14313fc83094
Abdul Jabbar Hafizh <jabbarworld1@gmail.com>
2022-04-17 05:36:50 +0700

    commit debug

--- graphql/fileUploads/file_upload.utilities.js
+++ graphql/fileUploads/file_upload.utilities.js
@@ -1,4 +1,7 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const fs = require('fs');
+
 const common = require('../../utils/common');
 
 /**
@@ -15,9 +18,12 @@ const SaveFileToServer = async (filename, createReadStream, customLocation, extN
     let random = common.create_UUID();
     let newFilename = `${random}-${filename}`;
     let location = `${uploadDir ? uploadDir : './public/fileuploads/'}${newFilename}${extName}`;
+    console.log('location', location);
 
     let stream = createReadStream()
       .on('error', (error) => {
+        console.log('error 1111', error);
+
         if (stream.truncated) {
           // delete the truncated file
           fs.unlinkSync(location);
@@ -25,7 +31,11 @@ const SaveFileToServer = async (filename, createReadStream, customLocation, extN
         reject(error);
       })
       .pipe(fs.createWriteStream(location))
-      .on('error', (error) => reject(error))
+      .on('error', (error) => {
+        console.log('error 2222', error);
+
+        reject(error);
+      })
       .on('finish', async () => {
         let file = {
           originalname: '',
@@ -34,6 +44,7 @@ const SaveFileToServer = async (filename, createReadStream, customLocation, extN
 
         file.buffer = fs.readFileSync(location);
         file.originalname = newFilename;
+        console.log('file', file);
 
         resolve(file);
       });
@@ -82,5 +93,46 @@ async function SaveFile(filename, createReadStream, custom_file_name = '', extNa
       });
   });
 }
+
+// const multerStorage = multer.memoryStorage();
+
+// const multerFilter = (req, file, callback) => {
+//   if (file.mimetype.startsWith('image')) {
+//     callback(null, true);
+//   } else {
+//     callback(new Error('Not image file format'), false);
+//   }
+// };
+
+// const upload = multer({
+//   storage: multerStorage,
+//   fileFilter: multerFilter,
+// });
+
+// exports.uploadUserPhoto = upload.single('file');
+
+// exports.resizeUserPhoto = async (req, res, next) => {
+//   try {
+//     let user;
+//     // ini untuk create
+//     if (!req.params.id) {
+//       user = req.body;
+//     }
+
+//     req.file.filename = `user-${user.role}-${user.NIP}.jpeg`.replace(/\s/g, '');
+
+//     await sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({ quality: 90 }).toFile(`public/fileuploads/${req.file.filename}`);
+
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// async function removeImageFromServer(bucket_file_name) {
+//   fs.unlink(`public/fileuploads/${bucket_file_name}`, (err) => {
+//     console.error(err);
+//   });
+// }
 
 module.exports = { SaveFileToServer, SaveFile };
